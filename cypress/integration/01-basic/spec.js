3b322f2ce4262514097013ef457e455a6bb11414
Gustavo Viruega <gustavoviruega@gmail.com>
2020-08-20 14:45:39 -0300

    Cypress workshop - Module 01: finished.

--- cypress/integration/01-basic/spec.js
+++ cypress/integration/01-basic/spec.js
@@ -6,12 +6,23 @@ it('loads', () => {
 
   // passing assertions
   // https://on.cypress.io/get
+  // GV: can't take screenshot of this nested element .screenshot() because ...
   cy.get('.new-todo').get('footer')
+  // GV: takes screenshot of this element and saves it on the temp folder with the name "input.png"
+  cy.get('.new-todo').screenshot('temp/input')
 
   // this assertion fails on purpose
   // can you fix it?
   // https://on.cypress.io/get
-  cy.contains('h1', 'todos')
+  // GV: added a timeout of 1 sec
+  // GV: take screenshot of the element and saves it with the name of the test "loads.png"
+  cy.contains('h1', 'todos', { timeout: 1000 }).screenshot()
+
+  // GV: using a custom data attribute
+  cy.contains('[data-cy=app-title]', 'todos')
+
+  // GV: take screenshot of the entire page
+  cy.screenshot({ capture: 'fullPage' })
 
   // can you write "cy.contains" using regular expression?
   // cy.contains('h1', /.../)
